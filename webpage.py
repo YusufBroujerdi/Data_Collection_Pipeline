c155ee13951954b25ff7074b0837c8ecf67f8a95
unknown <yusufbroujerdi@outlook.com>
2022-09-19 22:25:13 +0100

    Adds test for button clicking.

--- webpage.py
+++ webpage.py
@@ -2,6 +2,8 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 import time
 from selenium.webdriver.support.ui import Select
+from bs4 import BeautifulSoup
+import requests
 
 
 class Scraper:
@@ -35,6 +37,10 @@ class Scraper:
                     break
                 except:
                     pass
+                
+                if time.time() > starting_time + 10:
+                    print(f"Failed to press button (identified by {identification}) with identification {button}")
+                    break
           
                 
 
@@ -45,12 +51,15 @@ class Scraper:
     
     def scroll_to_bottom(self):
         
+        last_height = self.driver.execute_script("return document.body.scrollHeight")
+
         while True:
 
-            self.driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+            self.driver.execute_script("window.scrollTo(100, document.body.scrollHeight);")
             time.sleep(1)
 
-            new_height = driver.execute_script("return document.body.scrollHeight")
+            new_height = self.driver.execute_script("return document.body.scrollHeight")
+            
             if new_height == last_height:
                 break
             last_height = new_height
@@ -64,3 +73,33 @@ if __name__ == '__main__':
     lego.cookie_buttons = ['//*[@id="__next"]/div[5]/div/div/div[1]/div[1]/div/button',
                            '/html/body/div[6]/div/aside/div/div/div[3]/div[1]/button[1]']
     lego.remove_cookies()
+    
+    lego.driver.get('https://www.lego.com/en-gb/categories/age-1-plus-years')
+    buttons = ['//*[@id="product-facet-productType-accordion-content"]/div/div/ul/li[1]/label/div',
+               '/html/body/div[1]/main/div/div[4]/div/div/section/div/div[2]/div/div/a']
+    lego.click_buttons(buttons)
+    lego.scroll_to_bottom()
+    print('done')
+    
+    #To be removed soon --
+    
+    # //*[@id="bltc6cdf6e15ea6c848"]/section/div/div[2]/div/div/a
+    
+    # [@id="bltb4ff2489704d2385"]//*[@id="bltb4ff2489704d2385"]/section/div/div[2]/div/div
+    
+    # //*[@id="blt0723e5915b29f00d"]/section/div/div[2]/div/div
+    
+    # //*[@id="blt0723e5915b29f00d"]/section/div/div[2]/div
+
+    
+    # ProductListingsstyles__ProductsWrapper-sc-1taio5c-2 dFBaNn
+    
+    # ProductListingsstyles__ProductsWrapper-sc-1taio5c-2 dFBaNn
+    
+    # ProductListingsstyles__ProductsWrapper-sc-1taio5c-2 dFBaNn
+    
+    
+    
+    # ProductListingsstyles__ProductsWrapper-sc-1taio5c-2 dFBaNn
+    
+    # LinksNextstyles__AnchorButton-sc-1sxojvh-1 kARNPN Paginationstyles__ShowAllLink-npbsev-13 iWkTte
