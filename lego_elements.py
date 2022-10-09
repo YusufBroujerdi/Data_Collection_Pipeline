d01ddded4f91e52403cbe2f83130c7572bed38c6
unknown <yusufbroujerdi@outlook.com>
2022-10-09 19:17:45 +0100

    Adds methods to harvest details and creates LegoScraper subclass.

--- lego_elements.py
+++ lego_elements.py
@@ -2,7 +2,7 @@ dictionary = {
     #Front page elements
     'age_check_overlay' : {
         'condition' : lambda tag : tag.has_attr('class') and tag['class'] == ['AgeGatestyles__Wrapper-xudtvj-0', 'itkEkg'],
-        'dependency' : 'front_page'
+        'dependency' : 'webpage'
     },
     
     'age_check_button' : {
@@ -12,14 +12,14 @@ dictionary = {
     
     'cookie_accept_button' : {
         'condition' : lambda tag : tag.name == 'button' and tag.get_text() == 'Accept All',
-        'dependency' : 'front_page'
+        'dependency' : 'webpage'
     },
     
     #Product list elements
     'sets_checkbox_and_label' : {
         'condition' : lambda tag : tag.has_attr('data-test') and tag['data-test'] == 'checkbox-label' and \
             tag.find_all(lambda child : child.get_text().count('Sets') == 1) != [],
-        'dependency' : 'product_list'
+        'dependency' : 'webpage'
     },
     
     'sets_checkbox' : {
@@ -29,17 +29,17 @@ dictionary = {
     
     'page_info' : {
         'condition' : lambda tag : tag.has_attr('class') and tag['class'] == ['ProductGridstyles__Grid-lc2zkx-0', 'eGsxII'],
-        'dependency' : 'product_list'
+        'dependency' : 'webpage'
     },
     
     'show_all_button' : {
         'condition' : lambda tag : tag.has_attr('class') and tag['class'] == ['LinksNextstyles__AnchorButton-sc-1sxojvh-1', 'kARNPN', 'Paginationstyles__ShowAllLink-npbsev-13', 'iWkTte'],
-        'dependency' : 'product_list'
+        'dependency' : 'webpage'
     },
     
     'page_bottom_post_show_all' : {
         'condition' : lambda tag : tag.has_attr('class') and tag['class'] == ['Scrollstyles__Container-sc-1370r7z-0', 'kvnbhA'],
-        'dependency' : 'product_list'
+        'dependency' : 'webpage'
     },
     
     'showing_x_of_y_text' : {
@@ -49,15 +49,39 @@ dictionary = {
     
     'survey_window' : {
         'condition' : lambda tag : tag.has_attr('id') and tag['id'] == 'IPEdContainer',
-        'dependency' : 'product_list'
+        'dependency' : 'webpage'
     },
     
     'survey_window_no' : {
         'condition' : lambda tag : tag.has_attr('id') and tag['id'] == 'noButton',
         'dependency' : 'survey_window'
-    }
+    },
     
     #Details page elements
+    'item_description' : {
+        'condition' : lambda tag : tag.has_attr('class') and tag['class'] == ['ProductDetailsPagestyles__ProductDynamicContentContainer-sc-1waehzg-6', 'iHKzHW'],
+        'dependency' : 'webpage'
+    },
+    
+    'item_stats' : {
+        'condition' : lambda tag : tag.has_attr('class') and tag['class'] == ['ProductAttributesstyles__Container-sc-1sfk910-0', 'ctVkib'],
+        'dependency' : 'webpage'
+    },
+    
+    'item_rating' : {
+        'condition' : lambda tag : tag.has_attr('class') and tag['class'] == ['ProductOverviewstyles__ProductBadgesRow-sc-1a1az6h-0', 'bCQoZx'],
+        'dependency' : 'webpage'
+    },
+    
+    'item_price' : {
+        'condition' : lambda tag : tag.has_attr('class') and tag['class'] == ['ProductOverviewstyles__PriceAvailabilityWrapper-sc-1a1az6h-10', 'fCBTFc'],
+        'dependency' : 'webpage'
+    },
+    
+    'item_pictures' : {
+        'condition' : lambda tag : tag.has_attr('class') and tag['class'] == ['MediaQuery__MediaHideable-sc-1poqfy2-0', 'fjfTGa', 'ProductMediaViewerstyles__MediaQueryThumbnailsHorizontal-sc-13pkbbe-2', 'gnVtlp'],
+        'dependency' : 'webpage'
+    }
 }
 
 
@@ -84,3 +108,21 @@ product_list_links = ['https://www.lego.com/en-au/categories/age-1-plus-years',
 #     'condition' : lambda tag : tag.has_attr('class') and tag['class'] == ['Scrollstyles__Container-sc-1370r7z-0', 'kvnbhA'],
 #     'dependency' : 'page_info_and_bottom'
 # }
+
+#'/html/body/div[1]/main'
+
+#'/html/body/div[1]/main/div'
+
+#description '/html/body/div[1]/main/div/div[2]/div/div/div'
+#'ProductDetailsPagestyles__ProductDynamicContentContainer-sc-1waehzg-6', 'iHKzHW'
+
+#stats: '/html/body/div[1]/main/div/div[1]/div/ div[1]/div[2]/div'
+#'ProductAttributesstyles__Container-sc-1sfk910-0', 'ctVkib'
+
+#rates: '/html/body/div[1]/main/div/div[1]/div/ div[2]/div[1]'
+#'ProductOverviewstyles__ProductBadgesRow-sc-1a1az6h-0', 'bCQoZx'
+
+#price: '/html/body/div[1]/main/div/div[1]/div/ div[2]/div[3]'
+#'ProductOverviewstyles__PriceAvailabilityWrapper-sc-1a1az6h-10', 'fCBTFc'
+
+#pictures: 'ThumbnailsTrackstyles__Track-sc-141n2vw-4', 'dHIUWk'
--- webpage.py
+++ webpage.py
@@ -3,6 +3,7 @@ from selenium.webdriver.common.by import By
 import time
 from bs4 import BeautifulSoup
 import lego_elements as l
+import uuid
 
 
 class Scraper:
@@ -10,9 +11,7 @@ class Scraper:
     
     def __init__(self, URL, webpage_name):
         
-        options = webdriver.ChromeOptions()
-        options.add_experimental_option("prefs", {"profile.managed_default_content_settings.images": 2})
-        self.driver = webdriver.Chrome(chrome_options = options)
+        self.driver = webdriver.Chrome()
         self.elements = dict()
         self.driver.get(URL)
         self.webpage = webpage_name
@@ -37,13 +36,6 @@ class Scraper:
     
     
     
-    def click_buttons(self, buttons):
-        
-        for button_name in buttons:
-            self.click_button(button_name)
-    
-    
-    
     def wait_for(self, element_name, appear = 'appear', period = 10, print_warning = True):
         
         starting_time = time.time()
@@ -59,6 +51,14 @@ class Scraper:
                 if print_warning:
                     print(f"waited {period} seconds for element {element_name} to {appear}. But it never did.")
                 return False
+    
+    
+    
+    def wait_for_then_click(self, buttons, period = 10, print_warning = True):
+            
+        for button_name in buttons:
+            if self.wait_for(button_name, period = period, print_warning = print_warning):
+                self.click_button(button_name)
     
     
     
@@ -78,6 +78,31 @@ class Scraper:
             links.append(link_element.get('href'))
         
         return links
+    
+    
+    
+    def harvest_image_sources(self, element_name, condition = lambda tag : tag.name == 'img'):
+        
+        print(self.elements[element_name])
+        element = self.find_element_soup(element_name)
+        src_names = []
+        
+        for picture_element in element.find_all(condition):
+            src_names.append(picture_element.get('src').split('?')[0])
+        
+        return src_names
+    
+    
+    
+    def harvest_text_from_elements(self, element_list):
+        
+        text = dict()
+        
+        for element_name in element_list:
+            element = self.find_element_soup(element_name)
+            text[element_name] = element.get_text(separator = '¬')
+        
+        return text
     
     
     
@@ -106,13 +131,12 @@ class Scraper:
     
     def find_element_soup(self, element_name):
         
+        if element_name == 'webpage':
+            return BeautifulSoup(self.driver.execute_script("return document.documentElement.outerHTML;"), 'html.parser')
+        
         element = self.elements[element_name]
-        
-        if element['dependency'] == self.webpage:
-            parent = BeautifulSoup(self.driver.execute_script("return document.documentElement.outerHTML;"), 'html.parser')
-        else:
-            parent = self.find_element_soup(element['dependency'])
-        
+        parent = self.find_element_soup(element['dependency'])
+
         return parent.find(element['condition'])
     
     derive_xpath = lambda self, element_name : self.build_xpath(self.find_element_soup(element_name))
@@ -123,28 +147,109 @@ class Scraper:
 
 
 
+class LegoScraper(Scraper):
+    
+    
+    def __init__(self):
+        super().__init___(l.front_page_link, 'front_page')
+        self.elements = l.dictionary
+        self.setup()
+        #self.links = self.get_lego_links()
+    
+    
+    
+    def setup(self):
+
+        lego.wait_for('age_check_overlay')
+        lego.click_buttons(['age_check_button', 'cookie_accept_button'])
+    
+    
+    
+    def get_lego_links(self):
+        
+        details_page_links = []
+        for product_list_link in l.product_list_links:
+            
+            self.navigate(product_list_link, 'product_list')
+            
+            if self.wait_for('survey_window', period = 5, print_warning = False):
+                self.click_button('survey_window_no')
+            
+            self.click_button('sets_checkbox')
+            self.wait_for('show_all_button')
+            self.click_button('show_all_button')
+            
+            self.wait_for('page_bottom_post_show_all')
+            condition = lambda element : element.get_text().split()[1] == element.get_text().split()[-1]
+            #above condition checks if x == y in "showing_x_of_y_text" element.
+            self.scroll_to_bottom('page_bottom_post_show_all', 'showing_x_of_y_text', condition)
+            
+            condition = lambda element : element.has_attr('data-test') and element['data-test'] == 'product-leaf-title-link'
+            #above condition specifies those elements which have a text link.
+            details_page_links = details_page_links + self.harvest_links('page_info', condition)
+        
+        return(details_page_links)
+    
+    
+    
+    def expand_lego_data(self, text, img_links):
+        
+        
+        item_stats = text['item_stats'].split('¬')
+        item_rating = text['item_rating'].split('¬')
+        item_price = text['item_price'].split('¬')
+        
+        return {'ID' : item_stats[6],
+                'Name' : item_rating[0],
+                'Price' : item_price[1],
+                'Age' : item_stats[0],
+                'Pieces' : item_stats[2],
+                'Average Rating' : item_rating[1],
+                'Number of Ratings' : item_rating[3],
+                'Description' : text['item_description'],
+                'Image Links' : img_links,
+                'UUID' : uuid.uuid4()
+        }
+    
+    
+    
+    def store_lego_data(self):
+        pass
+
 if __name__ == '__main__':
     
-    lego = Scraper(l.front_page_link, 'front_page')
-    lego.elements = l.dictionary
-    lego.wait_for('age_check_overlay')
-    lego.click_buttons(['age_check_button', 'cookie_accept_button'])
+    setup_lego()
+    lego.navigate('https://www.lego.com/en-gb/product/90-years-of-play-11021', 'details_page')
     
-    details_page_links = []
-    for product_list_link in l.product_list_links:
-        
-        lego.navigate(product_list_link, 'product_list')
-        if lego.wait_for('survey_window', period = 5, print_warning = False):
-            lego.click_button('survey_window_no')
-        lego.click_button('sets_checkbox')
-        lego.wait_for('show_all_button')
-        lego.click_button('show_all_button')
-        
-        lego.wait_for('page_bottom_post_show_all')
-        condition = lambda element : element.get_text().split()[1] == element.get_text().split()[-1]
-        lego.scroll_to_bottom('page_bottom_post_show_all', 'showing_x_of_y_text', condition)
-        
-        condition = lambda tag : tag.has_attr('data-test') and tag['data-test'] == 'product-leaf-title-link'
-        details_page_links = details_page_links + lego.harvest_links('page_info', condition)
+    page = lego.find_element_soup('webpage')
     
-    print(details_page_links)
+    text = []
+    counter = 0
+    
+    baddies = ['All rights reserved', 'Skip to main content', 'Play Zone', 'ShopDiscoverHelp', 'Sets by themeAgesPrice']
+    
+    if lego.wait_for('survey_window', period = 5, print_warning = False):
+        lego.click_button('survey_window_no')
+    
+    imgs = lego.harvest_image_sources('item_pictures')
+    text = lego.harvest_text_from_elements(['item_description', 'item_stats', 'item_rating', 'item_price'])
+    product_data = expand_lego_text(text, imgs)
+    print(product_data)
+
+    # imgs = lego.harvest_image_sources('item_pictures')
+    # for img in imgs:
+    #     print(img)
+    
+    # for element in page.find_all():
+    #     check = sum([element.get_text().count(baddie) for baddie in baddies])
+    #     if check != 0 or element.get_text() == '':
+    #         continue
+    #     text.append((lego.build_xpath(element), element.get_text(separator = '¬')))
+    #     counter += 1
+    #     if counter == 500:
+    #         break
+    
+    # f = open('output.txt', 'w', encoding = 'utf-8')
+    # for x in set(text):
+    #     f.write(f'\n{x}')
+    # f.close()
